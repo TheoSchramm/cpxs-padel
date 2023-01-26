51fcc834d86fd1052f245573892bfad018374a62
Théo Pata Schramm <105531805+TheoSchramm@users.noreply.github.com>
2023-01-26 01:31:27 -0300

    Update reWinder.py

--- reWinder.py
+++ reWinder.py
@@ -1,54 +1,52 @@
-def __main__():
-    from cv2 import VideoCapture, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH, VideoWriter_fourcc, imshow, waitKey, VideoWriter, destroyAllWindows
-    from os import makedirs, path
-    from pathlib import Path
-    from datetime import datetime
+from os import makedirs, path
+from pathlib import Path
+from datetime import datetime
 
-    # Define o obj de captura de video e suas proporções
-    cap = VideoCapture(0)
-    width= int(cap.get(CAP_PROP_FRAME_WIDTH))
-    height= int(cap.get(CAP_PROP_FRAME_HEIGHT))
-    # Define o codec do obj de armazenamento de video
-    fourcc = VideoWriter_fourcc('m', 'p', '4', 'v')
+import cv2
 
-    # Lista que servirá como cache dos últimos 900 frames
-    frame_list = []
-    frame_limit = 30*30
+class VideoRecorder:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.cap.set(cv2.CAP_PROP_CONVERT_RGB, 1)
+        self.width = 640
+        self.height = 480
 
-    # Define o diretório padrão para as gravações
-    default_path = f'{Path.home()}\\Downloads\\CV2'
-    # Caso não tenha tal diretório, cria-se um
-    if not path.exists(default_path):
-        makedirs(default_path)
-
-    while True:
-        # Captura e mostra o frame atual
-        ret, frame = cap.read()
-
-        frame_list.append(frame)
-        if len(frame_list) > frame_limit:
-            frame_list.pop(0)
-
-        imshow(f'Pressione ESC para fechar ou R para gravar os ultimos 30 segundos', frame)
+        self.fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
         
-        # Um modo de parar a execução o programa
-        if waitKey(1) & 0xFF == 27: #esc
-            break
+        self.frame_list = []
+        self.frame_limit = 30*30
 
-        # Salva os últimos 900 frames capturados
-        if waitKey(1) & 0xFF in [ord('r'),ord('R')]:
-            print('>> Sys: Gravando últimos 30 segundos')
-            out = VideoWriter(f'{default_path}//{datetime.now().strftime("%d-%m-%y-%H-%M-%S")}.mp4', fourcc, 30.0, (width, height))
-            for frame in frame_list:
+        self.default_path = f'{Path.home()}\\Downloads\\CV2'
+        
+        if not path.exists(self.default_path):
+            makedirs(self.default_path)
+        
+    def mouse_listener(self, event, x, y, flags, param):
+        if event == cv2.EVENT_RBUTTONDOWN:
+            date_string = datetime.now().isoformat(timespec= 'seconds', sep='-').replace(':', '-')
+            out = cv2.VideoWriter(f'{self.default_path}//{date_string}.mp4', self.fourcc, 30.0, (self.width, self.height))
+            for frame in self.frame_list:
                 out.write(frame)
             out.release()
+    
+    def start(self):
+        win_name = "Botao direito do mouse para gravar os ultimos 30 segundos ou Esc para fechar o programa"
+        cv2.namedWindow(win_name)
+        cv2.setMouseCallback(win_name, self.mouse_listener)
+        
+        while True:
+            ret, frame = self.cap.read()
+            cv2.imshow(win_name, frame)
+            
+            self.frame_list.append(frame)
+            if len(self.frame_list) > self.frame_limit:
+                self.frame_list.pop(0)
+            
+            if cv2.waitKey(1) & 0xFF == 27: #esc
+                break
+        cv2.destroyAllWindows()
+        self.cap.release()
 
-    # Fecha todas as janelas abertas
-    destroyAllWindows()
-
-    # É necessário liberar o acesso desse dispositvo para evitar conflitos e ganhar + memória
-    cap.release()
-
-# É sempre bom fazer isso para evitar o script de rodar quando for importado para outros códigos
 if __name__ == '__main__':
-    __main__()
+    VideoRecorder().start()
